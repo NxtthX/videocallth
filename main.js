04226de52e63abd71df0dafa7ce72de186b8bcf9
NxtthX <110828120+NxtthX@users.noreply.github.com>
2022-08-09 09:37:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-verified = 'false'
-rdmnmbr = 6405780
-
-ThunkableWebviewerExtension.postMessage(rdmnmbr)
-
-function Checkforcode() {
-
-  userincode = document.getElementById('code').value
-  if (userincode == rdmnmbr) {
-    window.open("videocll.html");
-    document.getElementById('result').innerHTML = 'Logging you in'
+navigator.mediaDevices.getUserMedia({video: true})
+{
+  video: {
+    ...
+    facingMode: {
+      exact: 'environment'
+    }
   }
-  else
-    document.getElementById('result').innerHTML = 'Code Invalid'
-
-
 }
-function checkifverified() {
-
-
-  if (verified == "false") {
-
-    window.open("index.html", "_self");
-  }
